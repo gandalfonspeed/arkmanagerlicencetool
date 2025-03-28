70ea8d08e239290aac5f5a5662edc5ac94632f49
gandalfonspeed <Moritz@DESKTOP-3OGC7OM>
2025-03-28 15:54:01 +0100

    created project added licence reader added licence writer added instructions added qol feature: paste, clear, copy etc.

---
+++ arkmanager_licencetool/src/arkmanager_licencetool/Arkmanager_licencetool.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arkmanager_licencetool;
+
+/**
+ *
+ * @author gandalfonspeed
+ */
+
+public class Arkmanager_licencetool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        startup();
+    }
+    
+    
+    public static void startup(){
+        main_GUI main_gui = new main_GUI();
+        main_gui.setVisible(true);
+    }
+    
+    
+}
+
+
